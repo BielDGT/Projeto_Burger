a67995bb1df68e70de627020bb6050f0bf08ade2
BielDGT <david.tarleyy@gmail.com>
2025-02-06 11:56:30 -0300

    php com cards e header

---
+++ includes/header.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Pagina Pesquisa</title>
+    
+
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet"
+        integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+    <link rel="shortcut icon" href="./assets/fotos/logo_aba.png" type="image/x-icon">
+</head>
+
+<body>
+    <header>
+        <nav>
+            <a href="#" class="logo">
+                <img src="./assets/fotos/logo.png" alt="Logo" class="logo_img">
+                <h1>FoodFinder</h1>
+            </a>
+            <ul class="menu-icone">
+                <li><a href="#"><i class="bi bi-search"></i></a></li>
+                <li><a href="#"><i class="bi bi-house"></i></a></li>
+                <li><a href="#"><i class="bi bi-cart"></i></a></li>
+                <li><a href="#"><i class="bi bi-person-circle"></i></a></li>
+            </ul>
+        </nav>
+    </header>
+
--- pagina_pesquisa.php
+++ pagina_pesquisa.php
@@ -1,33 +1,6 @@
-<!DOCTYPE html>
-<html lang="pt-br">
+<?php include './includes/header.php' ?>
 
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Pagina Pesquisa</title>
-    
-
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet"
-        integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
-    <link rel="stylesheet" href="./assets/css/pagina_pesquisa.css">
-    <link rel="shortcut icon" href="./assets/fotos/logo_aba.png" type="image/x-icon">
-</head>
-
-<body>
-    <header>
-        <nav>
-            <a href="#" class="logo">
-                <img src="./assets/fotos/logo.png" alt="Logo" class="logo_img">
-                <h1>FoodFinder</h1>
-            </a>
-            <ul class="menu-icone">
-                <li><a href="#"><i class="bi bi-search"></i></a></li>
-                <li><a href="#"><i class="bi bi-house"></i></a></li>
-                <li><a href="#"><i class="bi bi-cart"></i></a></li>
-                <li><a href="#"><i class="bi bi-person-circle"></i></a></li>
-            </ul>
-        </nav>
-    </header>
+<link rel="stylesheet" href="./assets/css/pagina_pesquisa.css">
 
     <section id="menu">
         <nav>
@@ -121,6 +94,6 @@
     </footer>
 
 
-<body>
+</body>
 
 </html>
