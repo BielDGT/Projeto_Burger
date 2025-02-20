0b1f0db79ef4dfd411b532042bae70a86d36057d
Marcy_ <marciagisseli21@gmail.com>
2025-02-20 11:52:00 -0300

    criando o formulario da pagina administrador

---
+++ pagina_admin.php
@@ -0,0 +1,35 @@
+<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+<?php include './includes/header.php' ?>
+<link rel="stylesheet" href="./Assets/css/pagina_admin.css">
+
+</head>
+<body>
+    <main id="container-pagina-admin" class="text-center">
+    <section class="row-pagina-usuario" class="text-center">
+            <div class="foto-usuario">
+            <img src="./Assets/Fotos/pagina_usuario/usuario.png" alt="icone do usuário" class="foto-usuario">
+            </div>
+            <div class="btn-editar">
+                <ol>
+                    <li><a href="#">Editar</a></li>
+                </ol>
+            </div>
+            <div class="formulario">
+
+                <?php foreach ($resultado as $lista){?>
+
+                <form>
+                    <input type="text" class="formulario-campo" placeholder="Nome" <?php echo $lista['nome'] ?> ><br>
+                    <input type="text" class="formulario-campo" placeholder="Email" <?php echo $lista['senha'] ?>><br>
+                    <input type="text" class="formulario-campo" placeholder="Senha" <?php echo $lista['email'] ?>><br>
+                    <input type="text" class="formulario-campo" placeholder="CPF" <?php echo $lista['cep'] ?>><br>
+                    <input type="text" class="formulario-campo" placeholder="Endereço" <?php echo $lista['cpf'] ?>><br>
+                    <!-- <?php echo $lista['nome'] ?> -->
+                </form>
+
+                <?php }?>
+
+            </div>
+        </section>
+    </main>
+</body>
--- pagina_inicial.php
+++ pagina_inicial.php
@@ -1,8 +1,6 @@
-
-
-
 <link rel="stylesheet" href="./Assets/css/pagina_inicial.css">
 <link rel="stylesheet" href="./Assets/css/footer.css">
+<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
 <?php include './includes/header.php' ?>
 </head>
 <!-- Carrossel -->
@@ -43,97 +41,101 @@
 </section>
 <!-- carrossel -->
 <!-- COMEÇA AS OPÇOES DAS COMIDAS COM O BOTÃO DE VERMAIS -->
-<section class="opcoes-comidas">
-        <div class="row">
-            <div class="coluna">
-                <figure>
-                    <img src="./Assets/Fotos/pagina_inicial/pizza.jpg" alt="foto de uma pizza de pepperoni" class="foto">
-                    <figcaption>
-                        <h3>Pizza de pepperoni</h3>
-                    </figcaption>
-                </figure>
-                <ul class="btn-vermais">
-                    <li><a href="#">VER MAIS</a></li>
-                </ul>
-            </div>
-            <div class="coluna">
-                <figure>
-                    <img src="./Assets/Fotos/pagina_inicial/pizza.jpg" alt="foto de uma pizza de pepperoni" class="foto">
-                    <figcaption>
-                        <h3>Pizza de pepperoni</h3>
-                    </figcaption>
-                </figure>
-                <ul class="btn-vermais">
-                    <li><a href="#">VER MAIS</a></li>
-                </ul>
-            </div>
-            <div class="coluna">
-                <figure>
-                    <img src="./Assets/Fotos/pagina_inicial/pizza.jpg" alt="foto de uma pizza de pepperoni" class="foto">
-                    <figcaption>
-                        <h3>Pizza de pepperoni</h3>
-                    </figcaption>
-                </figure>
-                <ul class="btn-vermais">
-                    <li><a href="#">VER MAIS</a></li>
-                </ul>
-            </div>
-            <div class="coluna">
-                <figure>
-                    <img src="./Assets/Fotos/pagina_inicial/pizza.jpg" alt="foto de uma pizza de pepperoni" class="foto">
-                    <figcaption>
-                        <h3>Pizza de pepperoni</h3>
-                    </figcaption>
-                </figure>
-                <ul class="btn-vermais">
-                    <li><a href="#">VER MAIS</a></li>
-                </ul>
-            </div>
-            <div class="ccoluna">
-                <figure>
-                    <img src="./Assets/Fotos/pagina_inicial/pizza.jpg" alt="foto de uma pizza de pepperoni" class="foto">
-                    <figcaption>
-                        <h3>Pizza de pepperoni</h3>
-                    </figcaption>
-                </figure>
-                <ul class="btn-vermais">
-                    <li><a href="#">VER MAIS</a></li>
-                </ul>
-            </div>
-            <div class="coluna">
-                <figure>
-                    <img src="./Assets/Fotos/pagina_inicial/pizza.jpg" alt="foto de uma pizza de pepperoni" class="foto">
-                    <figcaption>
-                        <h3>Pizza de pepperoni</h3>
-                    </figcaption>
-                </figure>
-                <ul class="btn-vermais">
-                    <li><a href="#">VER MAIS</a></li>
-                </ul>
-            </div>
-            <div class="coluna">
-                <figure>
-                    <img src="./Assets/Fotos/pagina_inicial/pizza.jpg" alt="foto de uma pizza de pepperoni" class="foto">
-                    <figcaption>
-                        <h3>Pizza de pepperoni</h3>
-                    </figcaption>
-                </figure>
-                <ul class="btn-vermais">
-                    <li><a href="#">VER MAIS</a></li>
-                </ul>
-            </div>
-            <div class="coluna">
-                <figure>
-                    <img src="./Assets/Fotos/pagina_inicial/pizza.jpg" alt="foto de uma pizza de pepperoni" class="foto">
-                    <figcaption>
-                        <h3>Pizza de pepperoni</h3>
-                    </figcaption>
-                </figure>
-                <ul class="btn-vermais">
-                    <li><a href="#">VER MAIS</a></li>
-                </ul>
-            </div>
+<section class="container-cards text-center">
+    <div class="linha">
+        <div class="coluna">
+            <figure>
+                <img src="./Assets/Fotos/pagina_inicial/pizza.jpg" alt="foto de uma pizza de pepperoni" class="foto">
+                <figcaption>
+                    <h3>Pizza de pepperoni</h3>
+                    <ul class="btn-vermais">
+                        <li><a href="#">VER MAIS</a></li>
+                    </ul>
+                </figcaption>
+            </figure>
         </div>
+        <div class="coluna">
+            <figure>
+                <img src="./Assets/Fotos/pagina_inicial/pizza.jpg" alt="foto de uma pizza de pepperoni" class="foto">
+                <figcaption>
+                    <h3>Pizza de pepperoni</h3>
+                    <ul class="btn-vermais">
+                        <li><a href="#">VER MAIS</a></li>
+                    </ul>
+                </figcaption>
+            </figure>
+        </div>
+        <div class="coluna">
+            <figure>
+                <img src="./Assets/Fotos/pagina_inicial/pizza.jpg" alt="foto de uma pizza de pepperoni" class="foto">
+                <figcaption>
+                    <h3>Pizza de pepperoni</h3>
+                    <ul class="btn-vermais">
+                        <li><a href="#">VER MAIS</a></li>
+                    </ul>
+                </figcaption>
+            </figure>
+        </div>
+        <div class="coluna">
+            <figure>
+                <img src="./Assets/Fotos/pagina_inicial/pizza.jpg" alt="foto de uma pizza de pepperoni" class="foto">
+                <figcaption>
+                    <h3>Pizza de pepperoni</h3>
+                    <ul class="btn-vermais">
+                        <li><a href="#">VER MAIS</a></li>
+                    </ul>
+                </figcaption>
+            </figure>
+        </div>
+        <div class="linha">
+        <div class="coluna">
+            <figure>
+                <img src="./Assets/Fotos/pagina_inicial/pizza.jpg" alt="foto de uma pizza de pepperoni" class="foto">
+                <figcaption>
+                    <h3>Pizza de pepperoni</h3>
+                    <ul class="btn-vermais">
+                        <li><a href="#">VER MAIS</a></li>
+                    </ul>
+                </figcaption>
+            </figure>
+        </div>
+
+        <div class="coluna">
+            <figure>
+                <img src="./Assets/Fotos/pagina_inicial/pizza.jpg" alt="foto de uma pizza de pepperoni" class="foto">
+                <figcaption>
+                    <h3>Pizza de pepperoni</h3>
+                    <ul class="btn-vermais">
+                        <li><a href="#">VER MAIS</a></li>
+                    </ul>
+                </figcaption>
+            </figure>
+        </div>
+        <div class="coluna">
+            <figure>
+                <img src="./Assets/Fotos/pagina_inicial/pizza.jpg" alt="foto de uma pizza de pepperoni" class="foto">
+                <figcaption>
+                    <h3>Pizza de pepperoni</h3>
+                    <ul class="btn-vermais">
+                        <li><a href="#">VER MAIS</a></li>
+                    </ul>
+                </figcaption>
+            </figure>
+        </div>
+        <div class="coluna">
+            <figure>
+                <img src="./Assets/Fotos/pagina_inicial/pizza.jpg" alt="foto de uma pizza de pepperoni" class="foto">
+                <figcaption>
+                    <h3>Pizza de pepperoni</h3>
+                    <ul class="btn-vermais">
+                        <li><a href="#">VER MAIS</a></li>
+                    </ul>
+                </figcaption>
+            </figure>
+        </div>
+        </div>
+        
+    </div>
 </section>
 <!-- AQUI TERMINA AS OPÇOES DE COMIDA COM O BOTÃO DE VERMAIS -->
 </body>
