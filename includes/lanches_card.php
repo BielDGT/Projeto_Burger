1c8c86ab41fd28fa7ebfc34431e2d8b158f1529a
Marcy_ <marciagisseli21@gmail.com>
2025-03-04 22:07:35 -0300

    puxando as informações dos cards (imagem e nome ) com o banco

--- includes/lanches_card.php
+++ includes/lanches_card.php
@@ -1,14 +1,31 @@
 <link rel="stylesheet" href="../Assets/css/comidas_cards.css">
 
-<div class="coluna">
+<?php
+
+$dsn = 'mysql:dbname=db_damaju;host=127.0.0.1';
+$user = 'root';
+$password = '';
+
+$banco = new PDO($dsn, $user, $password);
+
+$select = 'SELECT * FROM tb_produtos';
+
+$resultado = $banco->query($select)->fetchAll();
+
+?>
+ <?php foreach ($resultado as $linha) { ?>
+    <div class="coluna">
     <figure>
         <!-- se começa primeiro a tag  figure para prossegir com o figcapture  -->
-        <img src="./Assets/Fotos/pagina_inicial/pizza.jpg" alt="imagem da comida" class="foto-produto">
+        <!-- <img src="./Assets/Fotos/pagina_inicial/<?php echo $linha ['Imagem']  ?>" alt="imagem da comida" > -->
         <figcaption>
-            <h3>Pizza de pepperoni</h3>
+            <h3><?php echo $linha ['nome']  ?></h3>
             <ul class="btn-vermais">
                 <li><a href="#">VER MAIS</a></li>
             </ul>
         </figcaption>
     </figure>
 </div>
+<?php } ?>
+
+
