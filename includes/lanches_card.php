7f1c79a4964d429521f89e53153ca7106b148ce6
Marcy_ <marciagisseli21@gmail.com>
2025-03-10 09:07:24 -0300

    fazendo aparecer as informações aleatorio

--- includes/lanches_card.php
+++ includes/lanches_card.php
@@ -1,9 +1,14 @@
-
 <link rel="stylesheet" href="../Assets/css/comidas_cards.css">
 
+<style>
+    img {
+        width: 200px;
+        height: 200px;
+    }
+</style>
 <?php
 
-$dsn = 'mysql:dbname=db_damaju;host=127.0.0.1';
+$dsn = 'mysql:dbname=db_damaju1;host=127.0.0.1';
 $user = 'root';
 $password = '';
 
@@ -14,17 +19,18 @@ $select = 'SELECT * FROM tb_produtos';
 $resultado = $banco->query($select)->fetchAll();
 
 ?>
- <?php foreach ($resultado as $linha) { ?>
+<?php foreach ($resultado as $linha) { ?>
     <div class="coluna">
-    <figure>
-        <!-- se começa primeiro a tag  figure para prossegir com o figcapture  -->
-        <!-- <img src="./Assets/Fotos/pagina_inicial/<?php echo $linha ['Imagem']  ?>" alt="imagem da comida" > -->
-        <figcaption>
-            <h3><?php echo $linha ['nome']  ?></h3>
-            <ul class="btn-vermais">
-                <li><a href="#">VER MAIS</a></li>
-            </ul>
-        </figcaption>
-    </figure>
-</div>
+        <figure>
+            <!-- se começa primeiro a tag  figure para prossegir com o figcapture  -->
+            <img src="./Assets/Fotos/pagina_inicial/comida.jpg" alt="imagem da comida">
+            <figcaption>
+                <h3><?php echo $linha['nome']  ?></h3>
+                <ul class="btn-vermais">
+                    <li><a href="#">VER MAIS</a></li>
+                </ul>
+            </figcaption>
+        </figure>
+    </div>
+
 <?php } ?>
