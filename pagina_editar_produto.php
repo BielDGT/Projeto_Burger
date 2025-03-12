b69175f22f69f68a30002c3b24b84ad09173129f
Josue <tomiokaorellana@gmail.com>
2025-03-12 09:33:12 -0300

    php de pagina cadastrar e editar terminado

---
+++ pagina_editar_produto.php
@@ -0,0 +1,48 @@
+<link rel="stylesheet" href="./Assets/css/pagina_cad_editar_produto.css">
+<?php include './includes/header.php';
+
+
+$id_produto_editar = $_GET['id_produto_editar'];
+
+$dsn = 'mysql:dbname=db_damaju;host=127.0.0.1';
+$user = 'root';
+$password = '';
+
+$banco = new PDO($dsn, $user, $password);
+
+$select = 'SELECT * FROM tb_produtos WHERE id_produtos = ' . $id_produto_editar;
+
+$dados = $banco->query($select)->fetch();
+
+?>
+
+
+
+<div class="container-cadastrar-editar">
+<h2>Editar Produto</h2>
+
+<form action="./produto_editar.php" method="POST">
+
+<input type="hidden"  placeholder="Nome" name="id" value="<?php echo $id_produto_editar ?>">
+
+    <input type="text"  placeholder="Nome" name="nome" value="<?=$dados['nome']?>">
+
+    <input type="text"  placeholder="Descrição" name="descricao" value="<?=$dados['descrição']?>">
+<div class="linha">
+    <input type="text"  placeholder="Categoria" name="categoria" value="<?=$dados['categoria']?>">
+
+    <input type="text"  placeholder="Valor" name="valor" value="<?=$dados['valor']?>">
+</div>
+    
+
+
+    <button type="submit" class="btn btn-success">Salvar</button>
+</form>
+
+
+</div>
+
+
+
+
+<?php include './includes/footer.php' ?>
