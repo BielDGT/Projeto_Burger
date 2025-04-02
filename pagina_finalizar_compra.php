53b3502be18830015e1ae8abda00b79e10b75b1e
Marcy_ <marciagisseli21@gmail.com>
2025-04-02 11:46:50 -0300

    finalizar compra

--- pagina_finalizar_compra.php
+++ pagina_finalizar_compra.php
@@ -2,37 +2,38 @@
 include './includes/header.php';
 
 $dsn = 'mysql:dbname=db_damaju;host=127.0.0.1';
- 
+
 $user = 'root';
- 
+
 $password = '';
- 
+
 $banco = new PDO($dsn, $user, $password);
- 
+
 $id_pessoa_imprimir = $_SESSION['id_pessoa'];
- 
+
 $script = 'SELECT
 tb_pessoa.nome,
 tb_produtos.nome,
 tb_produtos.descrição,
 tb_produtos.img,
-tb_carrinho.quantidade
+tb_carrinho.quantidade,
+tb_produtos.valor
 FROM tb_carrinho  
 INNER JOIN tb_pessoa ON tb_pessoa.id = tb_carrinho.id_pessoa
 INNER JOIN tb_produtos ON tb_produtos.id_produtos = tb_carrinho.id_produto
 WHERE tb_carrinho.id_pessoa = ' . $id_pessoa_imprimir;
- 
- 
- 
+
+
+
 // 'SELECT tb_pessoa.*, tb_produtos.*, tb_carrinho.*
 // FROM tb_carrinho
 // INNER JOIN tb_pessoa ON tb_pessoa.id = tb_carrinho.id_pessoa
 // INNER JOIN tb_produtos ON tb_produtos.id_produtos = tb_carrinho.id_produto
 // WHERE tb_carrinho.id_pessoa = ' . $id_pessoa_imprimir;
- 
- 
+
+
 $resultado = $banco->prepare($script);
- 
+
 $resultado->execute();
 ?>
 
@@ -43,11 +44,12 @@ $resultado->execute();
 <main>
     <div>
         <h3>Carrinho</h3>
+
         <table>
             <thead>
                 <tr>
                     <th>
-                        numeração
+                        foto
                     </th>
                     <th>
                         Comida
@@ -64,26 +66,25 @@ $resultado->execute();
                 </tr>
             </thead>
             <tbody>
-                <tr>
-                    <td>
-                        <?php echo $resultado['id'] ?>
-                    </td>
-                    <td>
-                        <?php echo $resultado['img'] ?>
-                    </td>
-                    <td>
-                        <?php echo $resultado['nome'] ?>
-                    </td>
-                    <td>
-                        <?php echo $resultado['valor'] ?>
-                    </td>
-                    <td>
-                        2x
-                    </td>
-                    <td>
-                        50,00
-                    </td>
-                </tr>
+                <?php foreach ($resultado as $linha) { ?>
+                    <tr>
+                        <td>
+                            <img src="./Assets/Fotos/fotos_cards/<?php echo $linha['img'] ?>" class="imagem-principal" alt="foto do produto">
+                        </td>
+                        <td>
+                        <?= $linha['nome'] ?>
+                        </td>
+                        <td>
+                        <?= $linha['valor'] ?>
+                        </td>
+                        <td>
+                            2x
+                        </td>
+                        <td>
+                            50,00
+                        </td>
+                    </tr>
+                <?php } ?>
             </tbody>
 
         </table>
@@ -120,8 +121,6 @@ $resultado->execute();
 
 
 </main>
-
-
 
 
 <?php include './includes/footer.php' ?>
