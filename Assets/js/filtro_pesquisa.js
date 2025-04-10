78d595fc6e139f48502d5997e495fe12ee085d79
Josue <tomiokaorellana@gmail.com>
2025-04-10 07:03:23 -0300

    observação para apresentação.

--- Assets/js/filtro_pesquisa.js
+++ Assets/js/filtro_pesquisa.js
@@ -3,29 +3,29 @@
         const produtos = document.querySelectorAll('.produtos .coluna');
 
         checkboxes.forEach(checkbox => {
-            checkbox.addEventListener('change', filterProdutos);
+            checkbox.addEventListener('change', filterProdutos); //chama a funcao
         });
 
         function filterProdutos() {
-            const selectedCategories = Array.from(checkboxes)
-                .filter(checkbox => checkbox.checked && checkbox.value !== 'Todos')
+            const selectedCategories = Array.from(checkboxes) //converte em um array
+                .filter(checkbox => checkbox.checked && checkbox.value !== 'Todos') //filtra e romove todos.
                 .map(checkbox => checkbox.value);
 
-            produtos.forEach(produto => {
-                const categoriaProduto = produto.dataset.categoria;
-                const mostrarProduto = selectedCategories.length === 0 || selectedCategories.includes(categoriaProduto);
+            produtos.forEach(produto => { //passa por card
+                const categoriaProduto = produto.dataset.categoria; //defini a cat dele
+                const mostrarProduto = selectedCategories.length === 0 || selectedCategories.includes(categoriaProduto); //Se pelo menos uma das condições for verdadeira mostrara marcado ou nao.
 
-                if (document.getElementById('todos').checked) {
-                    produto.style.display = 'block';
+                if (document.getElementById('todos').checked) { 
+                    produto.style.display = 'block';//vai exibir todos 
                 } else {
-                    produto.style.display = mostrarProduto ? 'block' : 'none';
+                    produto.style.display = mostrarProduto ? 'block' : 'none'; //vai aparecer o produto com  base na variavel mostrarproduto
                 }
             });
 
-            if (document.getElementById('todos').checked) {
-                checkboxes.forEach(checkbox => {
-                    if (checkbox.value !== 'Todos') {
-                        checkbox.checked = false;
+            if (document.getElementById('todos').checked) {//verifica se esta amrcado
+                checkboxes.forEach(checkbox => { 
+                    if (checkbox.value !== 'Todos') {//ignora o todos.
+                        checkbox.checked = false; //desmarca os outros
                     }
                 });
             }
